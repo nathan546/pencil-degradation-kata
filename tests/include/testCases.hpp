8d236927656249e6838beb6659f59ae618f2e3f1
Nathan Barrett Morrison <nathan546@gmail.com>
2018-02-04 11:37:38 -0500

    Added pencil erasing test case from Kata specifications

--- tests/include/testCases.hpp
+++ tests/include/testCases.hpp
@@ -54,6 +54,15 @@ TestSuite testSuites[] =
 			{"write", "She sells sea shells", "She sells sea shells"},
 			{"write", " down by the sea shore", "She sells sea shells down by the sea shore"}
 		}
+	),
+
+	//Write/erase test example from Kata guidelines
+	TestSuite(
+		{
+			{"write", "How much wood would a woodchuck chuck if a woodchuck could chuck wood?", "How much wood would a woodchuck chuck if a woodchuck could chuck wood?"},
+			{"erase", "chuck", "How much wood would a woodchuck chuck if a woodchuck could       wood?"},
+			{"erase", "chuck", "How much wood would a woodchuck chuck if a wood      could       wood?"}
+		}
 	)
 
 };
