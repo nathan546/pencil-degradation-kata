42299ec137066f01aef9600ccb3c2c632f3e28a1
Nathan Barrett Morrison <nathan546@gmail.com>
2018-02-04 19:33:51 -0500

    Added example production code

--- src/include/main.hpp
+++ src/include/main.hpp
@@ -2,6 +2,10 @@
 
 #define MAIN_HPP
 
-//...
+#define CHALKBOARD_PUNISHMENT_COUNT 10
+
+static const std::string repetitionPhrase = "Bart Simpson will not write production code prior to formulating a test case\r\n";
+
+static bool productionDemonstration();
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <string>
 #include <iostream>
 #include "main.hpp"
-
+#include "pencilFactory.hpp"
 
 int main (void){
-	std::cout << "Nothing to see here" << std::endl;
-	return 1;
+	return productionDemonstration();
+}
+
+static bool productionDemonstration(){
+	int writingRepetitions;
+	WritingBoard writingBoard;
+	Pencil * pencil;
+
+	unsigned int pencilLength = 1, pointDurability = 10000, eraserDurability = 10000;
+
+	pencil = new Pencil(pencilLength, pointDurability, eraserDurability);
+	pencil->setWritingBoard(&writingBoard);
+
+	for(writingRepetitions = 0; writingRepetitions < CHALKBOARD_PUNISHMENT_COUNT; writingRepetitions++){
+		pencil->writeText(repetitionPhrase);
+	}
+
+	pencil->eraseText("test case");
+
+	pencil->editText("...");
+
+	std::cout << pencil->readText() << std::endl;
+
+	delete pencil;
 }
